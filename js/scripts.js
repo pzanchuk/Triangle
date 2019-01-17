5b57764b4414e837c2837856b4e1898a7072f36c
Pavel Zanchuk and Yulia Shidlovskaya <yulia@>
2019-01-17 14:02:30 -0800

    create celebrity match webpage with branching base on age and gender, check empty boxes using Bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,61 @@
 $(document).ready(function(){
-  $("#formOne").submit(function(event){
 
-    var animalpick = $("input:radio[name=animals]:checked").val();
+  $("#main-form").submit(function(event){
 
-    if(animalpick === "turtles"){
-      $("#turtles").show();
-      $("#snakes").hide();
-      $("#insects").hide();
-    }else if(animalpick === "snakes"){
-      $("#turtles").hide();
-      $("#snakes").show();
-      $("#insects").hide();
-    }else{
-      $("#turtles").hide();
-      $("#snakes").hide();
-      $("#insects").show();
+    var inputName = $("input#name").val();
+    var inputAge = parseInt($("input#age").val());
+    var inputGender = $("#gender").val();
+    var inputWatch = $("input:radio[name=watch]:checked").val();
+
+    $("#age-control").removeClass("has-error");
+    $("#name-control").removeClass("has-error");
+
+    if ((inputName) && (inputAge)){
+
+      $(".your-name").text(inputName);
+
+      $(".result").show();
+      $("#1").hide();
+      $("#2").hide();
+      $("#3").hide();
+      $("#4").hide();
+      $("#5").hide();
+      $("#6").hide();
+      $("#7").hide();
+      $("#8").hide();
+      $("#9").hide();
+      $("#10").hide();
+      $("#11").hide();
+
+      if(inputAge <= 5){
+
+        $("#1").show();
+        $("#2").show();
+      }
+      else if(inputAge > 5 && inputGender === "1" ){
+        $("#4").show();
+        $("#10").show();
+      }
+      else if(inputAge > 5 && inputAge < 18) {
+        $("#10").show();
+        $("#4").show();
+        $("#9").show();
+      }else {
+        $("#5").show();
+        $("#10").show();
+        $("#11").show();
+      }
     }
+    else {
+      alert("Please fill in missing information");
+      if (!inputAge){
+        $("#age-control").addClass("has-error");
+      }
+      if (!inputName) {
+        $("#name-control").addClass("has-error");
+      }
+    }
+
     event.preventDefault();
   });
 });
